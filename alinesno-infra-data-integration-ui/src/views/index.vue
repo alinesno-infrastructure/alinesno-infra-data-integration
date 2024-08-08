843a3e52dc5a9fdf91abcaa814696f49f5e8f60d
王磊(Levi) <leviAip@gmail.com>
2024-08-07 22:13:00 -0400

    优化index.vue页面布局，添加应用信息头部展示及集群信息显示。

--- alinesno-infra-data-integration-ui/src/views/index.vue
+++ alinesno-infra-data-integration-ui/src/views/index.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="app-container acp-dashboard">
 
-    <!-- 应用统计 -->
-    <OperationWorkspaceCountStaitcs />
+    <el-row>
+      <el-col :span="24">
+        <div class="aip-appinfo-header">
+          <div class="header-icon-banner">
+            <i class="fa-brands fa-teamspeak"></i>
+          </div>
+          <div class="icon">
+            <div class="title">
+                {{ currentEnvClusterObj.appName }}
+            </div>
+            <div class="title-desc">
+                  {{ currentEnvClusterObj.desc }}
+            </div>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
 
     <!-- 应用套件和状态 -->
     <OperationWorkspaceService />
@@ -15,9 +30,91 @@
 
 <script setup name="Index">
 
-
-import OperationWorkspaceCountStaitcs from './operation-workspace/countStatics.vue'
 import OperationWorkspaceApps from './operation-workspace/apps.vue'
 import OperationWorkspaceService from './operation-workspace/service.vue'
 
+const currentEnvClusterObj = ref({
+  appName: '异构数据集成服务',
+  clusterName: 'Heterogeneous Data Integration Service',
+  desc: '多种异构数据源集成数据，放置到统一数据仓库，从不同系统间的数据采集管理。'
+});
+
 </script>
+
+
+<style lang="scss" scoped>
+.aip-appinfo-header{
+  position: relative;
+  padding: 20px;
+  overflow: hidden;
+
+  .header-icon-banner {
+    float: left;
+    font-size: 2.4rem;
+    margin-right: 10px;
+    color: #3b5998;
+  }
+
+  .head-app-status{
+    float: right;
+    font-size: 14px;
+    line-height: 1.7rem;
+    margin-bottom: 10px;
+
+    .integrated-item-box {
+
+      margin-left: 10px;
+      float: right;
+
+      ul , li {
+        margin: 0px;
+        padding: 0px;
+        list-style: none;
+      }
+      li {
+        float:right ;
+        margin-right: 10px;
+
+        img {
+          border-radius: 5px;
+          width: 25px;
+        }
+      }
+
+    }
+  }
+
+  .cluster-info {
+    float: right;
+    position: relative;
+    font-size: 14px;
+    margin-left: 10px;
+    font-weight: bold;
+    margin-top: 10px;
+    color: #3b5998;
+
+    span {
+      margin-left: 20px;
+    }
+  }
+
+  .icon {
+    float: left;
+  }
+
+  .title {
+    display: block;
+    font-weight: 600;
+    font-style: normal;
+    font-size: 24px;
+    color: #242e42;
+    text-shadow: 0 4px 8px rgba(36, 46, 66, 0.1);
+    margin-bottom: 10px;
+  }
+
+  .title-desc {
+    color: #79879c !important;
+    font-size: 12px;
+  }
+}
+</style>
