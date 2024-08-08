1b3fa59bdf1fff520ea2462aa1f7fbaae76965b4
王磊(Levi) <leviAip@gmail.com>
2024-08-08 05:31:00 -0400

    优化展示内容，更新构建解决方案部分为应用场景描述。

--- alinesno-infra-data-integration-ui/src/views/operation-workspace/apps.vue
+++ alinesno-infra-data-integration-ui/src/views/operation-workspace/apps.vue
@@ -9,14 +9,15 @@
           <div class="panel-body" style="height: auto;position: relative;">
               <div class="direct-box">
                 <ul>
-                  <li class="box-item" v-for="item in directContent" :key="item">
+                  <li class="box-item" v-for="item in advantages" :key="item">
                     <div class="dire-panel">
                       <div class="panel-title">
-                        <img class="dire-panel-icon" :src="item.icon" :alt="item.name" />
-                        {{ item.name }} 
+                        <i class="dire-panel-icon" :class="item.icon" :alt="item.name" />
+                        {{ item.title }}
                       </div>
-                      <div class="panel-describe">通过简单几步，图中7种主流开发环境轻松部署，摆脱海量文档搜索之苦。</div>
-                      <div class="panel-tip">访问链接</div>
+                      <div class="panel-describe">
+                        {{ item.description }}
+                      </div>
                     </div>
                   </li>
                 </ul>
@@ -27,19 +28,18 @@
       <el-col :span="5">
         <div class="grid-content">
           <div class="panel-header">
-            <div class="header-title"><i class="fa-solid fa-link"></i> 构建解决方案</div>
+            <div class="header-title"><i class="fa-solid fa-link"></i> 应用场景</div>
           </div>
           <div class="panel-body" style="height: auto;position: relative;">
             <div class="acp-app-list">
               <ul>
-                <li class="app-items" style="width:100%" v-for="item in apps" :key="item">
-                  <div class="app-icon">
-                    <img :src="item.icon" :alt="item.name" />
-                  </div>
+                <li class="app-items" style="width:100%" v-for="item in selectedAlarmScenarios" :key="item">
                   <div class="app-info">
-                    <div class="app-item-title">{{ item.name }} <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    <div class="app-item-title">
+                      <i :class="item.icon" :alt="item.name" />
+                        {{ item.name }}
                     </div>
-                    <div class="app-item desc">{{ item.desc }}</div>
+                    <div class="app-item desc">{{ item.description }}</div>
                   </div>
                 </li>
               </ul>
@@ -55,20 +55,21 @@
 
 <script setup>
 
-const directContent = ref([
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/d88319dfa5d204f019b4284149886c59-7d586ea82f792b61a8c87de60565133d.svg', name: '快速搭建网站', desc: 'With EC2 (2 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/f5d2c00d40914bff4f82f29f9ef768bc-53a84099cf556710383a52b4612a8612.svg', name: '部署开发环境', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/3da5e8169d2f1426f99fbef54575fe96-6382cb2dfdd2f74c99bc8a64a338358e.svg', name: '搭建云上博客', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/fb0cde6228b21d89ec222b45efec54e7-0856e92285f4e7ed254b2588d1fe1829.svg', name: '搭建小程序', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/a5ffe5487f62ef75d8e5cf78c18525a5-d4867f9d4adcd749f0c5aff987232847.svg', name: '云上高可用架构', desc: 'With Route 53 (3 分钟)' },
-]);
+const advantages = [
+  { icon: 'fa-solid fa-shield-alt', title: '集成更丰富', description: '整合多种监控和异常检测系统，提供全面的告警管理解决方案，满足不同业务需求。' },
+  { icon: 'fa-solid fa-chart-line', title: '告警更精准', description: '通过智能算法和数据分析技术，实现对告警的准确识别和定位，减少误报率，提高告警的准确性。' },
+  { icon: 'fa-solid fa-clipboard-list', title: '协同更便捷', description: '提供协同工作平台，支持团队成员之间的快速沟通和合作，有效提升团队处理告警的效率和协同能力。' },
+  { icon: 'fa-solid fa-chart-bar', title: '闭环根因分析', description: '实现从告警生成到根因分析的全流程闭环管理，帮助快速定位和解决问题，缩短故障修复时间，提升系统稳定性。' },
+  { icon: 'fa-solid fa-cogs', title: '一站式管理平台', description: '提供统一的告警管理平台，集成监控、分析、处理等功能于一体，简化操作流程，提升用户体验。' },
+  { icon: 'fa-solid fa-users-cog', title: 'MTTR持续优化', description: '通过告警数据分析和优化流程，持续缩短故障响应时间，不断提升系统的稳定性和可靠性。' }
+];
 
-const apps = ref([
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/051de32597041e41f73b97d61c67a13b-9cbdaf85e3bcf29b656fdedd8e6d1305.svg', name: '构建 Web 应用程序', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/fb0cde6228b21d89ec222b45efec54e7-0856e92285f4e7ed254b2588d1fe1829.svg', name: '部署无服务器微服务', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/7177e919b32ad97825f95e902595014b-1594766d92813b5baeb706c453f91de0.svg', name: '使用虚拟服务器构建', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/fb0cde6228b21d89ec222b45efec54e7-0856e92285f4e7ed254b2588d1fe1829.svg', name: '开始迁移到 AIP', desc: 'With Route 53 (3 分钟)' },
-  { icon: 'https://d1by4p17n947rt.cloudfront.net/icon/9da5a168cf8194c8ee5ed192a443d563-674375b53bc8ae94f48cfdb5c81e8363.svg', name: '托管静态 Web 应用程序', desc: 'With Route 53 (3 分钟)' },
-]);
+
+const selectedAlarmScenarios = [
+  { icon: 'fa-solid fa-desktop', name: '应用运行告警', description: '监控和管理网络设备的告警信息，包括路由器、交换机等设备的异常状态和故障信息，确保网络正常运行。' },
+  { icon: 'fa-solid fa-server', name: '服务器告警', description: '实时监测服务器的性能和运行状态，及时发现服务器负载异常、存储空间不足等问题，并进行相应处理，保障服务器稳定运行。' },
+  { icon: 'fa-solid fa-database', name: '数据库告警', description: '对数据库系统进行监控和告警管理，及时发现数据库连接异常、查询性能下降等问题，保障数据的安全和可靠性。' },
+];
+
 
 </script>
