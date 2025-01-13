d1b835dc1f9787a6120fc781ccdec5866ab8a60b
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:18 +0800

    feat:在TransMapper接口中添加版本号注释和导入语句

--- alinesno-infra-data-integration-domain/src/main/java/com/alinesno/infra/data/integration/mapper/TransMapper.java
+++ alinesno-infra-data-integration-domain/src/main/java/com/alinesno/infra/data/integration/mapper/TransMapper.java
@@ -4,13 +4,14 @@ import com.alinesno.infra.common.facade.mapper.repository.IBaseMapper;
 import com.alinesno.infra.data.integration.dto.TransDto;
 import com.alinesno.infra.data.integration.entity.TransEntity;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 /**
  * 【请填写功能名称】Mapper接口
  *
  * @author paul
- * @date 2024年3月10日
+ * @version 1.0.0
  */
 @Repository
 public interface TransMapper extends IBaseMapper<TransEntity> {
