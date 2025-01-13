dad2a9eb7dc88b88a9925f2557b3246afc8ecf9d
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:17 +0800

    feat:更新 ICategoryService 接口，增加版本号

--- alinesno-infra-data-integration-facade/src/main/java/com/alinesno/infra/data/integration/service/ICategoryService.java
+++ alinesno-infra-data-integration-facade/src/main/java/com/alinesno/infra/data/integration/service/ICategoryService.java
@@ -8,7 +8,7 @@ import com.alinesno.infra.data.integration.entity.CategoryEntity;
  * 【请填写功能名称】Service接口
  *
  * @author paul
- * @date 2024年3月10日
+ * @version 1.0.0
  */
 public interface ICategoryService extends IBaseService<CategoryEntity>{
 }
