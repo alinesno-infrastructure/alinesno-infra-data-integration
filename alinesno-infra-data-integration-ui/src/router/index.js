20645267cadd270949b0fbb21dc73b2458c88c8c
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:18 +0800

    feat:启用并配置用户个人中心路由

--- alinesno-infra-data-integration-ui/src/router/index.js
+++ alinesno-infra-data-integration-ui/src/router/index.js
@@ -138,21 +138,21 @@ export const constantRoutes = [
 
 
     ]
-  }
-  // {
-    // path: '/user',
-    // component: Layout,
-    // hidden: true,
-    // redirect: 'noredirect',
-    // children: [
-      // {
-        // path: 'profile',
-        // component: () => import('@/views/system/user/profile/index'),
-        // name: 'Profile',
-        // meta: { title: '个人中心', icon: 'user' }
-      // }
-    // ]
-  // }
+  },
+   {
+     path: '/user',
+     component: Layout,
+     hidden: true,
+     redirect: 'noredirect',
+     children: [
+       {
+         path: 'profile',
+         component: () => import('@/views/system/user/profile/index'),
+         name: 'Profile',
+         meta: { title: '个人中心', icon: 'user' }
+       }
+     ]
+   }
 ]
 
 // 动态路由，基于用户权限动态去加载
