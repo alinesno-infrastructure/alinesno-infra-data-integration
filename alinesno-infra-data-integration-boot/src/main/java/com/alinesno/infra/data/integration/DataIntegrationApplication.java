f7762a4e284908e5ce824afbf08ce8f3f84f06fc
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:17 +0800

    fix:修正DataIntegrationApplication主方法泛型使用问题

--- alinesno-infra-data-integration-boot/src/main/java/com/alinesno/infra/data/integration/DataIntegrationApplication.java
+++ alinesno-infra-data-integration-boot/src/main/java/com/alinesno/infra/data/integration/DataIntegrationApplication.java
@@ -1,22 +1,17 @@
 package com.alinesno.infra.data.integration;
 
-import com.alinesno.infra.common.core.context.SpringContext;
-import com.alinesno.infra.common.facade.enable.EnableActable;
-import com.alinesno.infra.common.web.adapter.sso.enable.EnableInfraSsoApi;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 集成一个Java开发示例工具
  * @author paul
- * @date 2024年3月10日
+ * @version  1.0.0
  */
 @SpringBootApplication
 public class DataIntegrationApplication {
 
-	public static <T> void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(DataIntegrationApplication.class, args);
 	}
 
