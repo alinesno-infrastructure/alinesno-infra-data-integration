5f7129bcf3e59d3de1db09c4345f2251b9d1eeff
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:16 +0800

    feat:更新 IJobRunHisService 接口版本号及注释

--- alinesno-infra-data-integration-facade/src/main/java/com/alinesno/infra/data/integration/service/IJobRunHisService.java
+++ alinesno-infra-data-integration-facade/src/main/java/com/alinesno/infra/data/integration/service/IJobRunHisService.java
@@ -8,7 +8,7 @@ import com.alinesno.infra.data.integration.entity.JobRunHisEntity;
  * 【请填写功能名称】Service接口
  *
  * @author paul
- * @date 2024年3月10日
+ * @version 1.0.0
  */
 public interface IJobRunHisService extends IBaseService<JobRunHisEntity>{
 
