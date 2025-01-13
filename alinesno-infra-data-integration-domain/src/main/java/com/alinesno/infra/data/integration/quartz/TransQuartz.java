7398f6e68e681e79a8b472aadbe66bc8d5154ba9
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:18 +0800

    feat:新增 TransQuartz 类的日志记录及版本号更新

--- alinesno-infra-data-integration-domain/src/main/java/com/alinesno/infra/data/integration/quartz/TransQuartz.java
+++ alinesno-infra-data-integration-domain/src/main/java/com/alinesno/infra/data/integration/quartz/TransQuartz.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,7 +25,7 @@ import java.util.Map;
  * 转换定时任务执行器
  *
  * @author paul
- * @date 2024年3月10日
+ * @version 1.0.0
  */
 @Component
 public class TransQuartz implements BaseJob {
