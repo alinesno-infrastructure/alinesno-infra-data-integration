23dc6d58849bd5be7b784d3353dd0425ae8a878a
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:17 +0800

    fix:修改 AppMain.vue 样式，调整 overflow 属性以解决水平滚动条问题。

--- alinesno-infra-data-integration-ui/src/layout/components/AppMain.vue
+++ alinesno-infra-data-integration-ui/src/layout/components/AppMain.vue
@@ -25,7 +25,7 @@ const tagsViewStore = useTagsViewStore()
   height: calc(100vh - 90px);
   width: 100%;
   position: relative;
-  overflow: hidden;
+  overflow-x: hidden;
 }
 
 .fixed-header + .app-main {
