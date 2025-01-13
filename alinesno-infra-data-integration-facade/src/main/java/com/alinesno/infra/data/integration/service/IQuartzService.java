712867046bd092f37550329d569c0e5db8f38f6b
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:15 +0800

    feat:更新 IQuartzService 接口版本号及注释

--- alinesno-infra-data-integration-facade/src/main/java/com/alinesno/infra/data/integration/service/IQuartzService.java
+++ alinesno-infra-data-integration-facade/src/main/java/com/alinesno/infra/data/integration/service/IQuartzService.java
@@ -10,7 +10,7 @@ import java.util.List;
  * 【请填写功能名称】Service接口
  *
  * @author paul
- * @date 2024年3月10日
+ * @version 1.0.0
  */
 public interface IQuartzService extends IBaseService<QuartzEntity> {
 
