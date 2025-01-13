8ebd8516df629342e0858f88c6dd932f813e83cf
李伟(Michael) <michaelAip@gmail.com>
2025-01-14 00:26:18 +0800

    refactor: 移除全局变量引用并优化全局配置

--- alinesno-infra-data-integration-ui/src/main.js
+++ alinesno-infra-data-integration-ui/src/main.js
@@ -43,14 +43,7 @@ import TreeSelect from '@/components/TreeSelect'
 // 字典标签组件
 import DictTag from '@/components/DictTag'
 
-// 引用全局变量文件
-import GLOBAL_VAR from '@/api/global_variable.js'
-import GLOBAL_FUN from '@/api/global_function.js'
-
 const app = createApp(App)
-
-app.config.globalProperties.GLOBAL_VAR = GLOBAL_VAR
-app.config.globalProperties.GLOBAL_FUN = GLOBAL_FUN
 
 // 全局方法挂载
 app.config.globalProperties.useDict = useDict
